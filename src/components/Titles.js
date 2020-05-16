3b32c403f9664918a382a61eef537074a9fa9f77
James Cole <james.cole.kings@gmail.com>
2020-05-16 22:03:32 +0100

    First

---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+	<div>
+		<h1 className="title-container__title">James Tells You The Weather Of A Very Specific Area</h1>
+		<h3 className="title-container__subtitle">Enter Your Place Name Please</h3>
+	</div>
+);
+
+export default Titles;
